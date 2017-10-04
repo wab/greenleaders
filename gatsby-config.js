78255b1828bc3775b31bb7a243a17e566d6c4b87
wabdsgn <wab2com@gmail.com>
2017-10-04 21:25:34 +0200

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,37 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-next`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: false,
+            },
+          },
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `zn4n7b753qwt`,
+        accessToken: `544541f0fd22c2613ea611e30af0feccef88e4e31e789dc21af005fd000b186c`,
+      },
+    },
+  ],
 }
