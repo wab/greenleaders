a5ee80774eb74c55d5242b4385b2878dac9fd9f0
wabdsgn <wab2com@gmail.com>
2017-10-04 23:36:58 +0200

    init templates queries

--- src/templates/rubrique.js
+++ src/templates/rubrique.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const RubriqueTemplate = () => (
-  <div>
-    <h1>RubriqueTemplate</h1>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
-)
+const RubriqueTemplate = ({data}) => {
+  const {title, subcategories} = data.contentfulRubrique;
+  return (
+    <div>
+      <h1>{title}</h1>
+      {subcategories && (
+        <ul>
+          {subcategories.map((category, index) => (<li key={index}>{category.title}</li>))}
+        </ul>
+      )}
+      <Link to="/">Go back to the homepage</Link>
+    </div>
+  )
+}
 
 export default RubriqueTemplate
+
+export const query = graphql`
+  query RubriqueQuery($id: String) {
+    contentfulRubrique(id: {eq: $id}) {
+      id
+      title
+      subcategories {
+        slug
+        title
+        article {
+          id
+          title
+        }
+      }
+    }
+  }
+`
