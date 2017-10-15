ed12a9316ab15d02e4a99fd4f9ef89a95dc9f52a
wabdsgn <wab2com@gmail.com>
2017-10-15 21:54:47 +0200

    header & nav

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "gatsby-link";
+import Container from "../components/Container";
+import styled from "styled-components";
+import logo from "../assets/svg/logo-footer.svg";
+
+const MainFooter = styled.footer`
+  border-top: 1px solid rgba(47, 47, 48, 0.15);
+  padding: 1.5em 0;
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Footer = () => (
+  <MainFooter>
+    <img src={logo} alt="Yves Rocher" />
+  </MainFooter>
+);
+
+export default Footer;
