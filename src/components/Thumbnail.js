450b77cf83468c39c41b177a33bf604b6745247b
wabdsgn <wab2com@gmail.com>
2017-10-19 15:02:36 +0200

    post grid

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -2,18 +2,8 @@ import React from "react";
 
 const Thumbnail = ({ img, alt, caption }) => (
   <figure className="Thumbnail">
-    <img
-      style={{
-        height: img.height,
-        width: img.width
-      }}
-      src={img.src}
-      srcSet={img.srcSet}
-      alt={alt ? alt : ''}
-    />
-    {caption && (
-      <figcaption>{caption}</figcaption>
-    )}
+    <img src={img.src} srcSet={img.srcSet} alt={alt ? alt : ""} />
+    {caption && <figcaption>{caption}</figcaption>}
   </figure>
 );
 
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,12 +1,17 @@
 import React from "react";
+import _ from "lodash";
+import moment from "moment";
 import Link from "gatsby-link";
+import { Page, Row, Column } from "hedron";
 import Thumbnail from "../components/Thumbnail";
 import BackButton from "../components/BackButton";
 import PostExcerpt from "../components/PostExcerpt";
-import _ from "lodash";
+
+moment.locale("fr");
 
 const PostTemplate = ({ data }) => {
   const {
+    createdAt,
     title,
     subtitle,
     rubrique,
@@ -25,83 +30,96 @@ const PostTemplate = ({ data }) => {
   );
 
   return (
-    <article>
-      <header>
-        <BackButton />
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
-      </header>
+    <Page fluid>
+      <Row divisions={12}>
+        <Column lg={10} lgShift={1} md={12}>
+          <BackButton /> breadcrumb
+        </Column>
+      </Row>
+      <article>
+        <Row divisions={24}>
+          <Column lg={5} lgShift={4} md={6}>
+            {thumbnail && (
+              <Thumbnail
+                img={thumbnail.responsiveResolution}
+                alt={thumbAlt}
+                caption={thumbnail.description}
+              />
+            )}
+            {summary && (
+              <aside>
+                <p>en bref</p>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: summary.childMarkdownRemark.html
+                  }}
+                />
+              </aside>
+            )}
+          </Column>
+          <Column lg={10} mdShift={1} md={17}>
+            <header>
+              <time dateTime={createdAt}>{moment(createdAt).format("ll")}</time>
+              <h1>{title}</h1>
+              <h2>{subtitle}</h2>
+            </header>
+            {main && (
+              <div>
+                <p>in concreto</p>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: main.childMarkdownRemark.html
+                  }}
+                />
+              </div>
+            )}
 
-      {thumbnail && (
-        <Thumbnail
-          img={thumbnail.responsiveResolution}
-          alt={thumbAlt}
-          caption={thumbnail.description}
-        />
-      )}
-      {summary && (
-        <aside>
-          <p>en bref</p>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: summary.childMarkdownRemark.html
-            }}
-          />
-        </aside>
-      )}
+            {speech && (
+              <blockquote
+                dangerouslySetInnerHTML={{
+                  __html: speech.childMarkdownRemark.html
+                }}
+              />
+            )}
 
-      {main && (
-        <div>
-          <p>in concreto</p>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: main.childMarkdownRemark.html
-            }}
-          />
-        </div>
-      )}
+            {onTheme.length > 0 && (
+              <aside>
+                <h3>Sur le même thème</h3>
+                <ul>
+                  {onTheme.map((post, index) => (
+                    <li key={index}>
+                      <PostExcerpt post={post} />
+                    </li>
+                  ))}
+                </ul>
+              </aside>
+            )}
+          </Column>
+        </Row>
 
-      {speech && (
-        <blockquote
-          dangerouslySetInnerHTML={{
-            __html: speech.childMarkdownRemark.html
-          }}
-        />
-      )}
-
-
-      {onTheme.length > 0 && (
-        <aside>
-          <h3>Sur le même thème</h3>
+        <nav>
+          <h3>Autres articles</h3>
           <ul>
-            {onTheme.map((post, index) => (<li key={index}><PostExcerpt post={post} /></li>))}
+            {postIndex.previous && (
+              <li>
+                <Link to={`/article/${postIndex.previous.slug}`}>
+                  Article Précédent :<br />
+                  {postIndex.previous.title}
+                </Link>
+              </li>
+            )}
+            {postIndex.next && (
+              <li>
+                <Link to={`/article/${postIndex.next.slug}`}>
+                  Article Suivant :<br />
+                  {postIndex.next.title}
+                </Link>
+              </li>
+            )}
           </ul>
-        </aside>
-      )}
-
-      <nav>
-        <h3>Autres articles</h3>
-        <ul>
-          {postIndex.previous && (
-            <li>
-              <Link to={`/article/${postIndex.previous.slug}`}>
-                Article Précédent :<br />
-                {postIndex.previous.title}
-              </Link>
-            </li>
-          )}
-          {postIndex.next && (
-            <li>
-              <Link to={`/article/${postIndex.next.slug}`}>
-                Article Suivant :<br />
-                {postIndex.next.title}
-              </Link>
-            </li>
-          )}
-        </ul>
-      </nav>
-
-    </article>
+        </nav>
+      </article>
+    </Page>
   );
 };
 
@@ -111,6 +129,7 @@ export const query = graphql`
   query PostQuery($id: String) {
     contentfulArticle(id: { eq: $id }) {
       id
+      createdAt
       title
       subtitle
       thumbnail {
@@ -146,7 +165,7 @@ export const query = graphql`
           slug
           title
           thumbnail {
-            responsiveResolution(width: 250) {
+            responsiveResolution(width: 220, height: 220) {
               width
               height
               src
