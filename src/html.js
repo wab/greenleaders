afc913f5cdcd367a56794d610ee4b9ea34531b6a
wabdsgn <wab2com@gmail.com>
2017-10-20 14:31:08 +0200

    ajout favicons

---
+++ src/html.js
@@ -0,0 +1,60 @@
+import React from "react";
+import favicon16 from "./assets/favicons/favicon-32x32.png";
+import favicon32 from "./assets/favicons/favicon-32x32.png";
+import faviconAppletouch from "./assets/favicons/apple-touch-icon.png";
+import safariPinnedTab from "./assets/favicons/safari-pinned-tab.svg";
+
+let stylesStr;
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = class HTML extends React.Component {
+  render() {
+    let css;
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      );
+    }
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={faviconAppletouch}
+          />
+          <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+          <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+          <link rel="mask-icon" href={safariPinnedTab} color="#a1a93e" />
+          <meta name="theme-color" content="#ffffff" />
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
+};
