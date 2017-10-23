75a6e97d0b20b66709508fd2bdc6f5d35cadff3c
wabdsgn <wab2com@gmail.com>
2017-10-23 22:34:52 +0200

    correction cta

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,11 +5,13 @@ import globals from "../utils/globals";
 
 const Button = styled.button`
   background-color: ${rubriqueColor};
-  padding: 1rem 2rem;
+  padding: 1rem ${props => (props.large ? "3.5rem" : "2rem")};
   outline: 0;
   border: none;
   color: ${colors.white};
   cursor: pointer;
+  font-size: ${globals.sizes.small};
+  font-weight: bold;
 `;
 
 export default Button;
