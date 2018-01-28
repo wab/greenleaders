6b79da6d32d0aaca3519c11aa6d78094eb58dd77
wabdsgn <wab2com@gmail.com>
2018-01-28 21:51:50 +0100

    accueil

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from "gatsby-link";
 import moment from "moment";
 import { Page, Row, Column } from "hedron";
 import PostExcerpt from "../components/PostExcerpt";
+import ActuExcerpt from "../components/ActuExcerpt";
 import BlockLink from "../components/BlockLink";
 import QuestionForm from "../components/QuestionForm";
 import styled, { css, ThemeProvider } from "styled-components";
@@ -12,7 +13,7 @@ import { noBullet, rowMargin, position } from "../utils/mixins";
 moment.locale("fr");
 
 const Accueil = styled.div`
-  padding: ${globals.sizes.large} 0;
+  padding: ${globals.sizes.medium} 0;
 `;
 
 const RowWrapper = styled.div`
@@ -27,6 +28,12 @@ const PostItemTitle = styled.h3`
   line-height: 1.4;
   margin-bottom: 0.2rem;
 `;
+const SectionTitle = styled.h2`
+  color: ${colors.grey.base};
+  font-size: ${globals.sizes.base};
+  line-height: 1.4;
+  margin-bottom: ${props => props.withMargin ? '20px' : 0};
+`;
 const Date = styled.div`
   color: ${colors.grey.light};
   text-transform: uppercase;
@@ -39,19 +46,43 @@ const StyledPostItem = styled.article`
   border-bottom: 1px solid ${colors.grey.light};
 `;
 
-const PostItem = ({ post }) => (
-  <BlockLink to={`/${post.rubrique.slug}/${post.slug}`}>
-    <StyledPostItem>
-      <PostItemTitle>{post.title}</PostItemTitle>
-      <Date>{moment(post.createdAt).format("ll")}</Date>
-    </StyledPostItem>
-  </BlockLink>
-);
+const PostItem = ({ post, isActu }) => {
+  const path = isActu ? `actu` : post.rubrique.slug;
+  return (
+    <BlockLink to={`/${path}/${post.slug}`}>
+      <StyledPostItem>
+        <PostItemTitle>{post.title}</PostItemTitle>
+        <Date>{moment(post.createdAt).format("ll")}</Date>
+      </StyledPostItem>
+    </BlockLink>
+  );
+}
 
 const IndexPage = ({ data }) => {
   const posts = data.allContentfulArticle.edges;
+  const actus = data.allContentfulActus.edges;
   const focusedPosts = posts.filter((post, i) => i <= 3);
   const otherPosts = posts.filter((post, i) => i > 3);
+  const renderOtherPosts = otherPosts.length > 0 ? (
+    <PostList>
+      {otherPosts.map(({ node: post }, index) => (
+        <li key={index}>
+          <PostItem post={post} />
+        </li>
+      ))}
+    </PostList>
+  ) : (<p>Pas d'autre articles pour le moment !</p>);
+
+  const renderActus = actus.length > 0 ? (
+    <PostList>
+      {actus.map(({ node: post }, index) => (
+        <li key={index}>
+          <ActuExcerpt post={post} />
+        </li>
+      ))}
+    </PostList>
+  ) : (<p>Pas d'autre articles pour le moment !</p>);
+
   return (
     <ThemeProvider theme={{ rubrique: "home" }}>
       <Accueil>
@@ -59,6 +90,13 @@ const IndexPage = ({ data }) => {
           <Page fluid>
             <Row divisions={24}>
               <Column md={16} lgShift={1}>
+                <RowWrapper>
+                  <Row divisions={16}>
+                    <Column lg={15} md={16} lgShift={1}>
+                      <SectionTitle>Derniers articles…</SectionTitle>
+                    </Column>
+                  </Row>
+                </RowWrapper>
                 <RowWrapper>
                   <Row divisions={16}>
                     {focusedPosts.map(({ node: post }, index) => (
@@ -70,17 +108,8 @@ const IndexPage = ({ data }) => {
                 </RowWrapper>
               </Column>
               <Column lg={4} md={8} lgShift={1}>
-                {otherPosts.length > 0 ? (
-                  <PostList>
-                    {otherPosts.map(({ node: post }, index) => (
-                      <li key={index}>
-                        <PostItem post={post} />
-                      </li>
-                    ))}
-                  </PostList>
-                ) : (
-                  <p>Pas d'autre articles pour le moment !</p>
-                )}
+                <SectionTitle withMargin>Actualités</SectionTitle>
+                {renderActus}
               </Column>
             </Row>
             <Row divisions={12}>
@@ -127,6 +156,24 @@ export const query = graphql`
             title
             categorie {
               slug
+            }
+          }
+        }
+      }
+    }
+    allContentfulActus {
+      edges {
+        node {
+          createdAt
+          title
+          slug
+          thumbnail {
+            description
+            responsiveResolution(width: 400, height: 400) {
+              width
+              height
+              src
+              srcSet
             }
           }
         }
